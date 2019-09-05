9dc3b357455061550c95b3cb3c7bca1570c551a3
andrewhall <hallal17@wfu.edu>
2019-09-05 16:07:59 -0400

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+using namespace std;
+#include <vector>
+#include <string>
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
+    int board [3][3];
+    int numGuesses = 0;
+    string userInput;
+
+//test
+
+    cout << "Guess battleship location by inputting two values: a b " << endl;
+    cin >> userInput;
+
+    for(int r = 0; r < 3; r++){
+        for ( int c = 0 ; c < 3; c++){
+
+        }
+    }
+
+
+
+
     return 0;
 }
